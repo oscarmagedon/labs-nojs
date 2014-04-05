9f33c3e17ddaf3dd6e15984d4b728694a16b1c1d
Oscar D Vegas V <oscarmagedon@gmail.com>
2014-04-04 23:30:37 -0400

    updating

--- labs/Animations/changeColor.php
+++ labs/Animations/changeColor.php
@@ -1,30 +1,33 @@
 <style>
+
+/* General class to the div test */
 #animated {
     width  : 100px;
     height : 100px;
     background-color  : #900;
     text-align: center;
-    animation : myfirst 2s;
+    margin: 10px;
 }
 
+/* Mozilla Part */
+#animated {
+    animation : myfirst 2s;
+}
 @keyframes myfirst
 {
     from {background:red;}
     to {background:yellow;}
 }
-
-/*
--webkit-animation : myfirst 2s; /* Chrome, Safari, Opera
-*/
-
-/* Chrome, Safari, Opera */
-/*
+/* Chrome, Safari, Opera Part */
+#animated {
+    -webkit-animation : myfirst 2s;
+}
 @-webkit-keyframes myfirst
 {
     from {background:red;}
     to {background:yellow;}
 }
-*/
+/**/
 </style>
 <h2>Animation CSS3</h2>
 <a href="http://www.w3schools.com/css/css3_animations.asp" target="_blank">Link on W3C</a>
--- labs/Generals/instructions.php
+++ labs/Generals/instructions.php
@@ -1,29 +1,36 @@
-<div id="instructions">
-    <h2>Instructions: </h2>
-    <p>
-        <b>It's easy:</b>
-    <ol>
+<div style="width: 500px;">
+
+    <h2 class="first-title">Instructions: </h2>
+
+    <h3 class="second-title">It's easy:</h3>
+
+    <ol class="number-list">
         <li>Create a new file "newFileExample.php" from your IDE in the specified folder.</li>
         <li>Create its entry on labs/menu.php (array type)</li>
         <li>Done! Let's change and test!</li>
     </ol>
+
+    <h2 class="second-title">Standard Classes</h2>
+
+    <p class="text-wrapper">
+        We have here different default classes for common elements to show on
+        the labs, that way we can create examples with a friendly appereance
+        by default.
     </p>
+
+    <p class="text-wrapper">
+        We are using a HTML5 CSS reset, so, we have to set style to every single
+        element on the document. And if in any case we need to use one of these styles
+        we can rewrite their attributes on the fly.
+    </p>
+
+    <ul class="normal-list">
+        CLASSSES:
+        <li><b>text-wrapper (p):</b> To use text paragraphs to explain</li>
+        <li><b>first-title (h3):</b> To big titles</li>
+        <li><b>second-title (h2):</b> To medium titles</li>
+        <li><b>number-list (ol): </b> Ordered lists </li>
+        <li><b>normal-list (ol): </b> Unordered lists </li>
+    </ul>
+
 </div>
-<style>
-    #instructions h2{
-        font-size: 140%;
-        padding: 4px;
-    }
-    #instructions{
-        width: 500px;
-    }
-    #instructions p{
-        text-align: justify;
-        margin: 5px 0 20px 10px;
-        line-height: 140%;
-    }
-    #instructions ol {
-        margin-left: 10px;
-        padding: 3px 0 0 40px;
-    }
-</style>
