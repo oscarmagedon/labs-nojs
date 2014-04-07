46364e4544cadef9f885d143b9a881d84895f3da
Oscar Vegas <oscarmagedon@gmail.com>
2014-04-07 12:06:18 -0400

    new formats

--- labs/Animations/changeColor.php
+++ labs/Animations/changeColor.php
@@ -1,5 +1,7 @@
 <style>
-
+.text-wrapper {
+    width: 600px;
+}
 /* General class to the div test */
 #animated {
     width  : 100px;
@@ -7,33 +9,47 @@
     background-color  : #900;
     text-align: center;
     margin: 10px;
+    /* ANIMATION PART TO: Mozilla */
+    animation : myfirst 4s;
+    /* ANIMATION PART TO: Chrome, Safari, Opera*/
+    -webkit-animation : myfirst 4s;
 }
-
-/* Mozilla Part */
-#animated {
-    animation : myfirst 2s;
-}
+/* animation function to Mozilla */
 @keyframes myfirst
 {
-    from {background:red;}
-    to {background:yellow;}
+    from { background: red; }
+    to   { background: yellow; }
 }
-/* Chrome, Safari, Opera Part */
-#animated {
-    -webkit-animation : myfirst 2s;
-}
+/* animation function to Chrome, Safari, Opera */
 @-webkit-keyframes myfirst
 {
-    from {background:red;}
-    to {background:yellow;}
+    from { background: red; }
+    to   { background: yellow; }
 }
-/**/
 </style>
-<h2>Animation CSS3</h2>
-<a href="http://www.w3schools.com/css/css3_animations.asp" target="_blank">Link on W3C</a>
-<p>
-    Here the element changes from red to yellow on load page.
+<h2 class="first-title">Changing one Color to Another</h2>
+<p class="text-wrapper">
+    Here the element changes from Red to Yellow in 4 seconds on load page.
+</p>
+<div id="animated">Change Two Colors</div>
+<p class="text-wrapper">
+    Note that we have in our code a <b>div</b> element with id = <b>animated</b>. We give it some basic
+    class to see it. At the end of the properties on the CSS part, we can see two new CSS3 properties:
+</p>
+<ul class="normal-list">
+    <li>
+        <b>animation</b>: Used by mozilla, followed by the name of the <b>@keyframes</b>
+        we set with the animation properties.
+    </li>
+    <li>
+        <b>-webkit-animation</b>: Used by chorme, opera, safari, and does the same, followed by the name of
+        the <b>@-webkit-keyframes</b> we set with the animation properties.
+    </li>
+</ul>
+<p class="text-wrapper">
+
 </p>
 
-<div id="animated">MouseOver Color</div>
-
+<p>
+    <a href="http://www.w3schools.com/css/css3_animations.asp" target="_blank">Source on W3C here</a>
+</p>
--- labs/Animations/colorMouseOver.php
+++ labs/Animations/colorMouseOver.php
@@ -3,33 +3,39 @@
     width:100px;
     height:100px;
     background:red;
+    border-radius: 4px;
+    padding: 5px;
+    font-style: italic;
+    text-align: center;
+    cursor: wait;
 }
 #fullColor:hover {
-    /*   -webkit-animation: myfirst 5s;   Chrome, Safari, Opera */
+    /* Mozilla */
     animation: fourColors 4s;
+    /* Chrome, Safari, Opera;*/
+    -webkit-animation: fourColors 4s;
 }
-
+/* Mozilla */
 @keyframes fourColors
 {
-    0%   {background:red;}
-    25%  {background:yellow;}
-    50%  {background:blue;}
-    100% {background:green;}
+    0%   { background: red; }
+    25%  { background: yellow; }
+    50%  { background: blue; }
+    100% { background: green; }
 }
-
 /* Chrome, Safari, Opera */
-/*
 @-webkit-keyframes fourColors
 {
-    0%   {background:red;}
-    25%  {background:yellow;}
-    50%  {background:blue;}
-    100% {background:green;}
+    0%   { background: red; }
+    25%  { background: yellow; }
+    50%  { background: blue; }
+    100% { background: green; }
 }
-*/
 </style>
-<p>
-    Here, we're gonna give the div 4 colors in 2 secs each
+<h2 class="first-title">Changing 4 colors on mouse over</h2>
+<p class="text-wrapper">
+    Here, we're gonna give the div 4 colors in 2 secs each. Then it comes back to
+    red like the beginning.
 </p>
 
 <div id="fullColor">Please, mouseover it!</div>
