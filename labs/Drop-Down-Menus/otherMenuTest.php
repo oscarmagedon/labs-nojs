dace3eb88a37a5749c929696a6a784c27d85e368
Oscar Vegas <oscarmagedon@gmail.com>
2014-04-14 17:38:32 -0400

    new menu tested

--- labs/Drop-Down-Menus/otherMenuTest.php
+++ labs/Drop-Down-Menus/otherMenuTest.php
@@ -1,13 +1,13 @@
 <style>
     /* demo page styles */
-    .example {
+    #example {
         font-family: "Trebuchet MS",Arial,Helvetica,sans-serif;
-        position:relative;
+        float:left;
         background:#fff url('rss/img/background.jpg');
-        width:520px;
-        height:382px;
+        width:550px;
+        height:300px;
         border:1px #000 solid;
-        margin:20px auto;
+        margin: 10px;
         padding:15px;
         border-radius:3px;
         -moz-border-radius:3px;
@@ -16,126 +16,124 @@
 
     /* main menu styles */
 
-    .nav,.nav ul {
+    #nav ul {
         list-style:none;
-        margin:0;
-        padding:0;
-    }
-
-    .nav {
-        position:relative;
-    }
-
-    .nav ul {
+        margin: 0;
+        padding: 0;
         height:0;
-        left:0;
+        left: 25px;
         overflow:hidden;
         position:absolute;
-        top:46px;
+        top:47px;
     }
 
-    .nav li {
+    #nav li {
         float:left;
         position:relative;
     }
 
-    .nav li a {
-        -moz-transition:0.5s;
-        -o-transition:0.5s;
-        -webkit-transition:0.5s;
-        background-color:#7770B4;
-        border:1px solid #6E67A6;
-        color:#FFF;
-        display:block;
-        font-size:16px;
-        line-height:35px;
-        padding:5px 20px;
-        text-decoration:none;
-        transition:0.5s;
+    #nav li a {
+        transition: 0.5s;
+        -webkit-transition : 0.5s;
+        background-color: #7770B4;
+        border: 1px solid #6E67A6;
+        color: #FFF;
+        display: block;
+        line-height: 35px;
+        padding: 5px 10px;
+        text-decoration: none;
     }
 
-    .nav li:hover > a {
+    .only-tit{
+        font-style: italic;
+        cursor: default;
+    }
+
+    #nav li:hover > a {
         background:#8CCA33;
         border-color:#6E67A6;
-        color:#fff;
+        color:#000;
+        /* Standard syntax */
+        background-image: linear-gradient(90deg,rgba(255,255,255,0.5),rgba(255,255,25,0));
+        /* For Firefox 3.6 to 15 */
+        background-image: -moz-linear-gradient(90deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0));
+        /* For Chrome 5.1 to 6.0 */
+        background-image: -webkit-gradient(linear, 0% 95%, 0% 0%, from(rgba(255, 255, 255, 0.5)), to(rgba(255, 255, 255, 0)));
     }
 
-    .nav li:hover ul.subs {
+    #nav li:hover ul.subs {
         height:auto;
         width:180px;
     }
 
-    .nav ul li {
-        -moz-transition:0.5s;
-        -o-transition:0.5s;
+    #nav ul li {
+        transition:0.5s;
         -webkit-transition:0.5s;
         opacity:0;
-        transition:0.5s;
         width:100%;
     }
 
-    .nav li ul li {
-        -moz-transition-delay:0s;
-        -o-transition-delay:0s;
+    #nav li ul li {
         -webkit-transition-delay:0s;
         transition-delay:0s;
     }
 
-    .nav li:hover ul li {
-        opacity:1;
-        -moz-transition-delay:0.5s;
-        -o-transition-delay:0.5s;
-        -webkit-transition-delay:0.5s;
-        transition-delay:0.5s;
+    #nav li:hover ul li {
+        opacity: 1;
+        transition-delay:0.2s;
+        -webkit-transition-delay:0.2s;
     }
 
-    .nav ul li a {
-        background:#7770B4;
-        border-color:#6E67A6;
-        color:#fff;
-        line-height:1px;
-        -moz-transition:1.5s;
-        -o-transition:1.5s;
-        -webkit-transition:1.5s;
-        transition:1.5s;
+    #nav ul li a {
+        transition: 0.3s;
+        -webkit-transition: 0.3s;
+        background: #7770B4;
+        border-color: #6E67A6;
+        color: #fff;
+        line-height: 1px;
     }
 
-    .nav li:hover ul li a {
-        line-height:35px;
+    #nav li:hover ul li a {
+        line-height: 30px;
     }
 
-    .nav ul li a:hover {
-        background:#8CCA33;
-        background-image: -webkit-gradient(linear, 0% 0%, 0% 95%, from(rgba(255, 255, 255, 0.5)), to(rgba(255, 255, 255, 0)));
+    #nav ul li a:hover {
+        background: #8CCA33;
+        /* Standard syntax */
+        background-image: linear-gradient(-90deg,rgba(255,255,255,0.5),rgba(255,255,25,0));
+        /* For Firefox 3.6 to 15 */
         background-image: -moz-linear-gradient(-90deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0));
-        background-image: -o-linear-gradient(-90deg,rgba(255,255,255,0.5),rgba(255,255,25,0));
+        /* For Chrome 5.1 to 6.0 */
+        background-image: -webkit-gradient(linear, 0% 0%, 0% 95%, from(rgba(255, 255, 255, 0.5)), to(rgba(255, 255, 255, 0)));
+
     }
+
+
 </style>
-<div class="example">
-    <ul class="nav">
-        <li><a href="http://www.script-tutorials.com/">Home</a></li>
-        <li><a href="http://www.script-tutorials.com/">Tutorials</a>
+<div id="example">
+    <ul id="nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#" class="only-tit">Tutorials&nbsp;&nbsp;&nbsp;&nbsp;&#9660;</a>
             <ul class="subs">
-                <li><a href="http://www.script-tutorials.com/category/html-css/">HTML / CSS</a></li>
-                <li><a href="http://www.script-tutorials.com/category/jquery/">JS / jQuery</a></li>
-                <li><a href="http://www.script-tutorials.com/category/php/">PHP</a></li>
-                <li><a href="http://www.script-tutorials.com/category/mysql/">MySQL</a></li>
-                <li><a href="http://www.script-tutorials.com/category/xslt/">XSLT</a></li>
-                <li><a href="http://www.script-tutorials.com/category/ajax/">Ajax</a></li>
-                <li><a href="http://www.script-tutorials.com/category/html-css/">HTML / CSS</a></li>
+                <li><a href="#">HTML / CSS</a></li>
+                <li><a href="#">JS / jQuery</a></li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">MySQL</a></li>
+                <li><a href="#">XSLT</a></li>
+                <li><a href="#">Ajax</a></li>
+                <li><a href="#">HTML / CSS</a></li>
             </ul>
         </li>
-        <li><a href="http://www.script-tutorials.com/category/resources/">Resources</a>
+        <li><a href="#" class="only-tit">Resources&nbsp;&nbsp;&nbsp;&nbsp;&#9660;</a>
             <ul class="subs">
-                <li><a href="http://www.script-tutorials.com/category/php/">PHP</a></li>
-                <li><a href="http://www.script-tutorials.com/category/mysql/">MySQL</a></li>
-                <li><a href="http://www.script-tutorials.com/category/xslt/">XSLT</a></li>
-                <li><a href="http://www.script-tutorials.com/category/ajax/">Ajax</a></li>
-                <li><a href="http://www.script-tutorials.com/category/html-css/">HTML / CSS</a></li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">MySQL</a></li>
+                <li><a href="#">XSLT</a></li>
+                <li><a href="#">Ajax</a></li>
+                <li><a href="#">HTML / CSS</a></li>
             </ul>
         </li>
-        <li><a href="http://www.script-tutorials.com/about/">About</a></li>
-        <li><a href="http://www.script-tutorials.com/creating-css3-dropdown-menu-4/">Back</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Back</a></li>
     </ul>
-    <div style="clear:both"></div>
 </div>
